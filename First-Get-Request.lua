d5f9582e0f6406c11a7c293bd95c0804086a27c1
megabyte0x <yashygkf@gmail.com>
2024-04-27 21:22:00 +0530

    faet: First-Get-Request

---
+++ First-Get-Request.lua
@@ -0,0 +1,41 @@
+local json = require("json")
+
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+_0RBT_TOKEN = "BUhZLMwQ6yZHguLtJYA5lLUa9LQzLXMXRfaq9FVcPJc"
+
+FEE_AMOUNT = "1000000000000" -- 1 $0RBT
+BASE_URL = "https://dummyjson.com/products"
+
+ReceivedData = ReceivedData or {}
+
+--[[
+    Handler to send 1 $0RBT and a GET request to the `0rbit` process.
+]]
+Handlers.add(
+    "Get-Request",
+    Handlers.utils.hasMatchingTag("Action", "First-Get-Request"),
+    function(msg)
+        Send({
+            Target = _0RBT_TOKEN,
+            Action = "Transfer",
+            Recipient = _0RBIT,
+            Quantity = FEE_AMOUNT,
+            ["X-Url"] = BASE_URL,
+            ["X-Action"] = "Get-Real-Data"
+        })
+        print(Colors.green .. "You have sent a GET Request to the 0rbit process.")
+    end
+)
+
+--[[
+    Handler to receive the data from the `0rbit` process.
+]]
+Handlers.add(
+    "Receive-Data",
+    Handlers.utils.hasMatchingTag("Action", "Receive-Data-Feed"),
+    function(msg)
+        local res = json.decode(msg.Data)
+        ReceivedData = res
+        print(Colors.green .. "You have received the data from the 0rbit process.")
+    end
+)
