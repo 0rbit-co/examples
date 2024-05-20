e817d29fdd3045e2de41bf7a357d79646beff896
megabyte0x <yashygkf@gmail.com>
2024-05-21 03:26:09 +0530

    feat: First POST request

---
+++ First-Post-Request.lua
@@ -0,0 +1,57 @@
+local json = require("json")
+
+_0RBIT = "WSXUI2JjYUldJ7CKq9wE1MGwXs-ldzlUlHOQszwQe0s"
+_0RBT_TOKEN = "BUhZLMwQ6yZHguLtJYA5lLUa9LQzLXMXRfaq9FVcPJc"
+
+FEE_AMOUNT = "1000000000000" -- 1 $0RBT
+BASE_URL = "https://g8way.0rbit.co/graphql"
+-- The data body to be sent in the POST request
+BODY = json.encode({
+    query = [[
+                            query {
+                                transactions(
+                                    owners: ["vh-NTHVvlKZqRxc8LyyTNok65yQ55a_PJ1zWLb9G2JI"]
+                                ) {
+                                    edges {
+                                        node {
+                                            id
+                                        }
+                                    }
+                                }
+                            }
+                        ]]
+});
+
+ReceivedData = ReceivedData or {}
+
+--[[
+    Handler to send 1 $0RBT and a GET request to the `0rbit` process.
+]]
+Handlers.add(
+    "Get-Request",
+    Handlers.utils.hasMatchingTag("Action", "First-Get-Request"),
+    function(msg)
+        Send({
+            Target = _0RBT_TOKEN,
+            Action = "Transfer",
+            Recipient = _0RBIT,
+            Quantity = FEE_AMOUNT,
+            ["X-Url"] = BASE_URL,
+            ["X-Action"] = "Get-Real-Data"
+        })
+        print(Colors.green .. "You have sent a GET Request to the 0rbit process.")
+    end
+)
+
+--[[
+    Handler to receive the data from the `0rbit` process.
+]]
+Handlers.add(
+    "Receive-Data",
+    Handlers.utils.hasMatchingTag("Action", "Receive-Response"),
+    function(msg)
+        local res = json.decode(msg.Data)
+        ReceivedData = res
+        print(Colors.green .. "You have received the data from the 0rbit process.")
+    end
+)
