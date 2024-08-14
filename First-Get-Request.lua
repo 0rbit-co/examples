45c65c57a9f5f02d77ac4ce6d4528ab147be5c7e
Sarthak Shah <92942966+Not-Sarthak@users.noreply.github.com>
2024-08-14 20:14:54 +0530

    Update First-Get-Request.lua

--- First-Get-Request.lua
+++ First-Get-Request.lua
@@ -4,7 +4,7 @@ _0RBIT = "BaMK1dfayo75s3q1ow6AO64UDpD9SEFbeE8xYrY2fyQ"
 _0RBT_TOKEN = "BUhZLMwQ6yZHguLtJYA5lLUa9LQzLXMXRfaq9FVcPJc"
 
 FEE_AMOUNT = "1000000000000" -- 1 $0RBT
-BASE_URL = "https://dummyjson.com/products"
+BASE_URL = "https://api.diadata.org/v1/assetQuotation/Arweave/0x0000000000000000000000000000000000000000"
 
 ReceivedData = ReceivedData or {}
 
